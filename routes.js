9b95e52a187da7179f8e51f28fbf6a91933060f4
Pham Ho Phi <phamhophi05@gmail.com>
2022-10-02 23:25:55 +0700

    update lab 1.7

--- routes.js
+++ routes.js
@@ -37,4 +37,10 @@ const requestHandler = (req, res) => {
   res.end();
 };
 
-module.exports = requestHandler;
+// module.exports = requestHandler;
+// module.exports = {
+//   handler: requestHandler,
+//   someText: "Some hard coded text",
+// };
+module.exports.handler = requestHandler;
+module.exports.someText = "Some hard coded text";
