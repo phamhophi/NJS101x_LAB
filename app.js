2283ca85d21b788be0622339bcf5359eeecafa9f
Pham Ho Phi <phamhophi05@gmail.com>
2022-10-02 16:11:44 +0700

    update lab1.3

--- app.js
+++ app.js
@@ -6,7 +6,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  // process.exit()
+  res.setHeader("Content-Type", "tetx/html");
 });
 
 server.listen(3000);
