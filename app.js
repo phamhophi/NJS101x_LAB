d27ebd86f29d68aa404bb4dadc46c268384968ba
Pham Ho Phi <phamhophi05@gmail.com>
2022-10-02 16:58:11 +0700

    update lab 1.4

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
       "<body><form action='/message method='POST'><input type='text'><button type='submit'>Send</button></form></body>"
     );
     res.write("</html>");
-    res.end();
+    return res.end();
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
