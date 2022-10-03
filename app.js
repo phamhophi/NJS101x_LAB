85c6b29ffbfede3887c7375676c1cd2a873f806d
Pham Ho Phi <phamhophi05@gmail.com>
2022-10-03 21:43:32 +0700

    update lab2

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const http = require("http");
 const routes = require("./routes");
 
+const express = require("express");
+
+const app = express();
+
 console.log(routes.someText);
 const server = http.createServer(routes.handler);
 
