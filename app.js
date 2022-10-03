681b16031f2c868aedc230c18b9c377c2b71f149
Pham Ho Phi <phamhophi05@gmail.com>
2022-10-03 23:42:31 +0700

    update lab2

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always run!");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In the middleware");
   res.send(
     "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
   );
@@ -20,7 +17,6 @@ app.use("/product", (req, res, next) => {
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In the middleware");
   res.send("<h1>Hello from Express!</h1>");
 });
 
