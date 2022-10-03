d91a048110d4ed86a60d054b459372648d436d4a
Pham Ho Phi <phamhophi05@gmail.com>
2022-10-03 23:35:42 +0700

    update lab2

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ app.use("/", (req, res, next) => {
 
 app.use("/add-product", (req, res, next) => {
   console.log("In the middleware");
-  res.send("<h1>The 'Add Product' Page</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
