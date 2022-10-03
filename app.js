204d84b8233eef2ae417fe82355fc3e5f0922318
Pham Ho Phi <phamhophi05@gmail.com>
2022-10-03 22:52:59 +0700

    update lab2

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const app = express();
 
 app.use((req, res, next) => {
   console.log("In the middleware");
-  next();
+  next(); // Allows the request to continue to the next middleware in line
 });
 
 app.use((req, res, next) => {
   console.log("In the middleware");
+  //...
 });
 
 const server = http.createServer(app);
