b846027a1b4afa403d92885112169a08d1c10af6
Pham Ho Phi <phamhophi05@gmail.com>
2022-10-03 22:49:04 +0700

    update lab2

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const express = require("express");
 
 const app = express();
 
-console.log(routes.someText);
+app.use((req, res, next) => {
+  console.log("In the middleware");
+});
+
 const server = http.createServer(routes.handler);
 
 server.listen(3000);
