be714dd761283cb44a84b95c81f9d2c2f1c07c4a
Pham Ho Phi <phamhophi05@gmail.com>
2022-10-04 18:40:26 +0700

    update lab3

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+// app.engine(
+//   "hbs",
+//   expressHbs({
+//     layoutsDir: "views/layouts",
+//     defaultLayout: "main-layout",
+//     extname: "hbs",
+//   })
+// );
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
