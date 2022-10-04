9557c936ded04f6bed785bc0a98418c9e6e62fc4
Pham Ho Phi <phamhophi05@gmail.com>
2022-10-04 15:48:51 +0700

    update lab3

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
