af0c3e32c1516e78a9bca13e9ced64415f9cc70c
Pham Ho Phi <phamhophi05@gmail.com>
2022-10-06 16:30:10 +0700

    update lab4

--- app.js
+++ app.js
@@ -3,16 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const expressHbs = require("express-handlebars");
 
+const errorController = require("./controllers/error");
+
 const app = express();
 
-// app.engine(
-//   "hbs",
-//   expressHbs({
-//     layoutsDir: "views/layouts",
-//     defaultLayout: "main-layout",
-//     extname: "hbs",
-//   })
-// );
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -25,8 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
